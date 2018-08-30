1cb09837c08b5826da80e3ca0112c0f3e573e661
saurav <sauravpd58@gmail.com>
2018-08-30 09:47:22 +0530

    added Employee class

---
+++ src/main/java/XMLParser/JAXB/Employee.java
@@ -0,0 +1,47 @@
+package XMLParser.JAXB;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Employee 
+{
+	private String employeeName;
+	private int age;
+	private String position;
+	private long salary;
+
+	public String getEmployeeName() 
+	{
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) 
+	{
+		this.employeeName = employeeName;
+	}
+	public int getAge() 
+	{
+		return age;
+	}
+	public void setAge(int age) 
+	{
+		this.age = age;
+	}
+	public String getPosition() 
+	{
+		return position;
+	}
+	public void setPosition(String position) 
+	{
+		this.position = position;
+	}
+	public long getSalary() 
+	{
+		return salary;
+	}
+	public void setSalary(long salary) 
+	{
+		this.salary = salary;
+	}
+}
