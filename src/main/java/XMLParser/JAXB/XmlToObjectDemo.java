634c6efcccbf8ce5c9c7e346201ed23b16bc6a37
saurav <sauravpd58@gmail.com>
2018-08-30 09:47:41 +0530

    added XmlToObjectDemo class

---
+++ src/main/java/XMLParser/JAXB/XmlToObjectDemo.java
@@ -0,0 +1,26 @@
+package XMLParser.JAXB;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class XmlToObjectDemo 
+{
+	public static void main(String[] args) 
+	{
+		try {
+
+			File file = new File("src//main//java//XMLParser//JAXB//employee.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Employee employee = (Employee) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(employee.getEmployeeName());
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+}
